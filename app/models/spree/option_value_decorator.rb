2e4762b2e0360de84c99a83b299181a3e9f8a0c5
Mike Bourgeous <mike@mikebourgeous.com>
2015-07-07 15:35:45 -0600

    Use a database join to get products' variants, reducing round trips.

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,5 +1,7 @@
 Spree::OptionValue.class_eval do
 
-  scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_values_variants.variant_id IN (?)", product.variants_including_master.map(&:id)).joins(:variants) }
+  scope :for_product, ->(product){
+    joins(:variants).where('spree_variants.product_id' => product.try(:id)).uniq
+  }
 
 end
