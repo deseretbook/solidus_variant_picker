18f6bbed57b035b88f27ecac3bd2199b5eed4674
Mike Bourgeous <mike@mikebourgeous.com>
2015-07-07 18:01:44 -0600

    Sort variants by option value position after variant position.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,8 +2,17 @@ Spree::ProductsController.class_eval do
 
   before_filter :get_variant_param, only: [:show]
 
-  def get_variant_param
-    @variant_id = params[:variant_id] || nil
-  end
 
+  # Overrides Spree's default show method to order @variants by a different criteria
+  def show_with_variant_sort
+    show_without_variant_sort
+    @variants = @variants.order_by_option_value
+  end
+  alias_method_chain :show, :variant_sort
+
+
+  private
+    def get_variant_param
+      @variant_id = params[:variant_id] || nil
+    end
 end
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,5 +1,18 @@
 Spree::Variant.class_eval do
 
+  # Sorts by option value position and other criteria after variant position.
+  scope :order_by_option_value, ->{
+    includes(:option_values).references(:option_values).unscope(:order).order(
+      position: :asc
+    ).order(
+      'spree_option_values.position ASC'
+    ).order(
+      is_master: :desc,
+      id: :asc
+    ).uniq
+  }
+
+
   def first_option_value
     self.option_values.where(option_type_id: self.product.first_option_type.id).order(position: :asc).first
   end
