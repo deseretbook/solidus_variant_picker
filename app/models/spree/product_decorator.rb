27479873f026adc5f0b6a1a12393eaf093675422
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2015-07-09 17:00:57 -0600

    Use all option types in the option type header.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,4 +4,9 @@ Spree::Product.class_eval do
     self.option_types.order(position: :asc).first
   end
 
+  # Returns the presentation names of the product's option types as a sentence.
+  def option_types_presentation
+    self.option_types.order(position: :asc).pluck(:presentation).compact.map(&:pluralize).uniq.to_sentence
+  end
+
 end
