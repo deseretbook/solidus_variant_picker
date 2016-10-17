1039a691eb9f4a1a76097a374e133c6af3868d7c
Eric Saupe <ericsaupe@gmail.com>
2016-10-17 11:17:11 -0600

    Rubocop changes

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -20,10 +20,12 @@ Spree::Variant.class_eval do
   # Returns all option value delivery messages, if present, as a single String.
   # Returns nil if no option values have a delivery message.
   def option_delivery_messages
-    dmsg = option_values.map{|ov|
+    dmsg = option_values.map{ |ov|
       msg = ov.delivery_message
       msg.present? ? h(msg) : nil
-    }.compact.uniq.join('. ').html_safe
+    }.compact.uniq
+
+    dmsg = safe_join(dmsg, '. ')
 
     dmsg.present? ? dmsg : nil
   end
--- spec/controllers/spree/products_controller_spec.rb
+++ spec/controllers/spree/products_controller_spec.rb
@@ -6,7 +6,7 @@ describe Spree::ProductsController, type: :controller do
   let!(:variant) { create(:master_variant, product: product) }
 
   it 'should assign @variant_id if variant_id param is present' do
-    spree_get :show, id: product.to_param, variant_id: variant.id
+    get :show, params: { id: product.to_param, variant_id: variant.id }
     expect(assigns['variant_id'].to_i).to eq(variant.id)
   end
 
@@ -16,7 +16,7 @@ describe Spree::ProductsController, type: :controller do
       create(:base_variant, product: product) # this has option values
       create(:base_variant, product: product, option_values: [])
 
-      spree_get :show, id: product.to_param
+      get :show, params: { id: product.to_param }
       expect(assigns['variants'].count).to eq(1)
     end
   end
